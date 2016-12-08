36b448ee83c77823ad419e39b05cd177c569b6bd
GraemeB <grae22@gmail.com>
2016-12-08 09:23:25 +0200

    Adding & retrieving entities from Roadmap.

--- Core/Roadmap.cs
+++ Core/Roadmap.cs
@@ -1,8 +1,63 @@
-﻿using Roadmapp.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Roadmapp.Entities;
 
 namespace Roadmapp.Core
 {
   class Roadmap
   {
+    //-------------------------------------------------------------------------
+
+    // This Roadmap's entity factory.
+    private EntityFactory EntityFactory { get; set; } = new EntityFactory();
+
+    // Entities organised by type.
+    private Dictionary< Type, List< Entity > > Entities { get; set; }
+      = new Dictionary< Type, List< Entity > >();
+
+    //-------------------------------------------------------------------------
+
+    public T AddEntity< T >() where T : Entity
+    {
+      // Instantiate a new entity.
+      T entity = (T)EntityFactory.GetEntity< T >();
+
+      if( entity == null )
+      {
+        return null;
+      }
+
+      // Add it to our collection.
+      if( Entities.ContainsKey( typeof( T ) ) == false )
+      {
+        Entities.Add(
+          typeof( T ),
+          new List< Entity >() );
+      }
+
+      Entities[ typeof( T ) ].Add( entity );
+
+      return entity;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public void GetEntities< T >( out ReadOnlyCollection< T > entities ) where T : Entity
+    {
+      List< T > list = new List< T >();
+
+      if( Entities.ContainsKey( typeof( T ) ) )
+      {
+        foreach( Entity entity in Entities[ typeof( T ) ] )
+        {
+          list.Add( (T)entity );
+        }
+      }
+
+      entities = new ReadOnlyCollection< T >( list );
+    }
+
+    //-------------------------------------------------------------------------
   }
 }
--- Roadmap_Test/Core/Roadmap_Test.cs
+++ Roadmap_Test/Core/Roadmap_Test.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Roadmapp.Core;
+using Roadmapp_Test.Entities;
 
 namespace Roadmapp_Test.Core
 {
@@ -21,8 +23,23 @@ namespace Roadmapp_Test.Core
     //-------------------------------------------------------------------------
 
     [TestMethod]
-    public void TestMethod1()
+    public void AddEntities()
     {
+      // Add some entities.
+      EntityMocks.EntityMock1 mock1 = TestObject.AddEntity< EntityMocks.EntityMock1 >();
+      Assert.IsNotNull( mock1, "Failed to add entity." );
+
+      EntityMocks.EntityMock2 mock2 = TestObject.AddEntity< EntityMocks.EntityMock2 >();
+      Assert.IsNotNull( mock2, "Failed to add entity." );
+
+      // Check they're there.
+      ReadOnlyCollection< EntityMocks.EntityMock1 > mock1Entities;
+      TestObject.GetEntities< EntityMocks.EntityMock1 >( out mock1Entities );
+      Assert.IsTrue( mock1Entities.Contains( mock1 ), "Failed to retrieve entity." );
+
+      ReadOnlyCollection< EntityMocks.EntityMock2 > mock2Entities;
+      TestObject.GetEntities< EntityMocks.EntityMock2 >( out mock2Entities );
+      Assert.IsTrue( mock2Entities.Contains( mock2 ), "Failed to retrieve entity." );
     }
 
     //-------------------------------------------------------------------------
