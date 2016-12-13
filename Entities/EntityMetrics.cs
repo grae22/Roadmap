35e4de59aded81029935b267f77ef645c78bd4b8
GraemeB <grae22@gmail.com>
2016-12-13 13:30:17 +0200

    Adding EntityMetrics (WIP).

---
+++ Entities/EntityMetrics.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Roadmapp.Entities
+{
+  class EntityMetrics
+  {
+    //-------------------------------------------------------------------------
+
+    // The entity for which these metrics pertain to.
+    public Entity Entity { get; private set; }
+
+    // The number of entities that have either a direct or indirect
+    // dependency on his entity.
+    public uint DependantCount { get; private set; }
+
+    //-------------------------------------------------------------------------
+
+    public EntityMetrics( Entity entity )
+    {
+      Entity = entity;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public void Calculate()
+    {
+      CalculateDependantCount( this );
+    }
+
+    //-------------------------------------------------------------------------
+
+    private static void CalculateDependantCount( EntityMetrics metrics )
+    {
+      metrics.DependantCount = CountDependants( metrics.Entity );
+    }
+
+    //-------------------------------------------------------------------------
+
+    private static uint CountDependants( Entity entity )
+    {
+      uint count = 0;
+
+      List< Entity > dependants;
+      entity.GetDependants( out dependants );
+
+      foreach( Entity dependant in dependants )
+      {
+        count++;
+        count += CountDependants( dependant );
+      }
+
+      return count;
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
---
+++ Roadmap_Test/Entities/EntityMetrics_Test.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Roadmapp.Entities;
+
+namespace Roadmapp_Test.Entities
+{
+  [TestClass]
+  public class EntityMetrics_Test
+  {
+    //-------------------------------------------------------------------------
+
+    private EntityFactory Factory = new EntityFactory( new EntityRelationshipManager() );
+    private EntityMetrics TestObject { get; set; }
+
+    //-------------------------------------------------------------------------
+
+    [TestInitialize]
+    public void Initialise()
+    {
+      TestObject = new EntityMetrics( Factory.GetRealisationEntity() );
+      Assert.IsNotNull( TestObject, "Failed to instantiate TestObject." );
+
+      Factory.GetStrategyEntity().AddDependency( TestObject.Entity );
+
+      TestObject.Calculate();
+    }
+
+    //-------------------------------------------------------------------------
+
+    [TestMethod]
+    public void DependantCount()
+    {
+      Assert.AreEqual( 1U, TestObject.DependantCount, "Dependant count is incorrect." );
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
